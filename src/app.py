d2d0624d47ea42c6744e2d07fb6098e685b59fb7
Chaitanya Ashok Raj <Chaitanya.Raj@Geminisolutions.com>
2024-04-04 23:49:20 +0530

    fixes

--- src/app.py
+++ src/app.py
@@ -1,33 +1,53 @@
 import streamlit as st
+import os
 import yolo_video
 import video_crop
+
 def main():
-     st.title("Object Detection with YOLO")
-     st.write("Upload a video file to detect objects.")
-
+     st.title("StarzPlay POC")
+     temp_video_path = None
+     output_video_path = None
+     temp_video_path_2 = None
      uploaded_file = st.file_uploader("Upload video", type=["mp4", "avi", "mov", "wmv", "mkv"])
+     
+     placeholder = st.empty()
 
-     if uploaded_file is not None:
-          if st.button("Process Video"):
+     with placeholder.container():
+          if uploaded_file is not None:
                temp_video_path = "temp_video.mp4"
                with open(temp_video_path, "wb") as temp_file:
                     temp_file.write(uploaded_file.read())
-               print('hehehehe',temp_video_path)
-               temp_video_path_2 = video_crop.get_contour_remove_black_borders(temp_video_path)
-               print('hahahaha',temp_video_path_2)
-               output_video_path = video_crop.zoomed_to_fill_result_video(temp_video_path_2)
-               # output_video_path = yolo_video.yolo_video_bounding_create(temp_video_path)
-               
-               print("Printing output video path:\n",output_video_path)
 
-               st.write("Processed Video:")
-               processed_video_file = open(output_video_path, "rb").read()
-               st.video(processed_video_file)
+               st.write("Input Video:")
+               st.video(temp_video_path)
 
-               import os
+               if st.button("Process Video"):
+                    with st.spinner("Processing..."):
+                         try:
+                              temp_video_path_2 = video_crop.get_contour_remove_black_borders(temp_video_path)
+                              output_video_path = video_crop.zoomed_to_fill_result_video(temp_video_path_2)
+                              st.write("Processed Video:")
+                              processed_video_file = open(output_video_path, "rb").read()
+                              st.video(processed_video_file)
+                         except Exception as e:
+                              st.error(f"Error processing video: {e}")
+
+     if st.button("Clear"):
+          if temp_video_path is not None:
                os.remove(temp_video_path)
+               temp_video_path = None
+               print(temp_video_path_2)
+
+          if temp_video_path_2 is not None:
+               print(temp_video_path_2)
                os.remove(temp_video_path_2)
-               # os.remove(output_video_path)
+               temp_video_path_2 = None
+
+          if output_video_path is not None:
+               print(output_video_path)
+               os.remove(output_video_path)
+               output_video_path = None
+          placeholder.empty()
 
 if __name__ == "__main__":
-    main()
+     main()
